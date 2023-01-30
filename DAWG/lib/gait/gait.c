a1e8ad3d580b8edb936f14bef9604d1e46bc8e25
purk99 <dawid.wilczewski@gmx.de>
2023-01-30 20:18:29 +0100

    added functions to IK

--- DAWG/lib/gait/gait.c
+++ DAWG/lib/gait/gait.c
@@ -3,12 +3,14 @@
 //Funktion zur Erzeugung von Bezierkurven
 //für bestimmte Koordinate
 //zu bestimmtem Zeitpunkt
+
 //time € {0;1}
-int bezierCurveN3(int coordinate, int time){
+//coordinate: 0 = x; 1 = y
+int bezierCurveN3(int coordinate, int t){
     //%C(t) = (1-t)^3 * p0 + 3 * t * (1-t)^2 * p1 + 3 * t^2 * (1-t) * p2 + t^3 * p3;
     //P1 (-20|0) in Millimetern
     //P2 (-20|40)
     //P3 (20|40)
     //P4 (20|0)
-
+    
 }
---
+++ DAWG/lib/gait/gait.h
@@ -0,0 +1,10 @@
+///////////////////////////
+//save gait patterns here 
+//Walking
+
+int p1[2] = {-20, 0};
+int p2[2] = {-20, 40};
+int p3[2] = {20, 40};
+int p4[2] = {20; 0};
+
+int bezierCurve(int coordinate);
