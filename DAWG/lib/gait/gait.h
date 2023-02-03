0139b2a06829c321db44be8b0053c23322ce292a
purk99 <66315113+purk99@users.noreply.github.com>
2023-02-03 23:47:03 +0100

    ik not working

--- DAWG/lib/gait/gait.h
+++ DAWG/lib/gait/gait.h
@@ -2,17 +2,17 @@
 //save gait patterns here 
 //Walking
 
-#include "utils.h"
+//#include "utils.h"
 
 #ifndef GAIT_H
 #define GAIT_H
 
-int p1[2] = {-20, 0};
-int p2[2] = {-20, 40};
-int p3[2] = {20, 40};
-int p4[2] = {20, 0};
+int p1[2] = {0, 150};
+int p2[2] = {0, 190};
+int p3[2] = {50, 190};
+int p4[2] = {50, 150};
 
-int bezierCurveN3(int coordinate, int t);
+//int bezierCurveN3(int coordinate, int t);
 //void makeStep(int leg);
 
 #endif
